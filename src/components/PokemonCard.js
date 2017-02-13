fe21afc41379e97e1c6179c505065f2a2f4dff07
Anthony Stansbridge <github@anthonystansbridge.co.uk>
2017-02-13 15:42:28 +0000

    Updated PokemonCard with an active state via click handler

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import './PokemonCard.css';
 
-class PokemonCard extends Component {
-  render() {
-      return (<div className="PokemonCard"></div>);
-  }
+class PokemonCard extends Component { 
+    static defaultProps = {
+        onClick: Function.prototype,
+    };
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            active: false,
+        };
+    }
+
+    handleClick() {
+        this.setState({
+            active: !this.state.active,
+        });
+        this.props.onClick();
+    }
+
+    buildClassName() {
+        const base = "PokemonCard";
+        const active = base+"-active";
+        var classNames = [base];
+        if (this.state.active) {
+            classNames.push(active);
+        }
+        return classNames.join(' ');
+    }
+
+    render() {
+        return (<div className={this.buildClassName()} onClick={this.handleClick}></div>);
+    }
 }
 
 export default PokemonCard;
